5e71550d2d8a3ec9abd81ec2d9a93bce7529f901
Pritham Sriram G <prithamsriram@gmail.com>
2021-06-15 16:01:14 +0530

    modified forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :conversations
-  root to: 'home#index'
+  root to: 'ideas#index'
   resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
